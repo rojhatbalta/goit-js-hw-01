d52a0def93dec9930998cca4e95cd7e3a7014c37
rojhatbalta <rojhatbalta@gmail.com>
2024-06-29 20:16:27 +0300

    Homework update

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,11 @@
 function getElementWidth(content, padding, border) {
-  let totalWidth = content + padding * 2 + border * 2;
-  return totalWidth;
+  let cont = parseInt(content);
+  let padd = parseInt(padding);
+  let bord = parseInt(border);
+  let totalWidth = cont + padd * 2 + bord * 2;
+  return `${totalWidth}px`;
 }
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
